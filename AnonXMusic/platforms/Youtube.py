423bd01c7209f25ffae708156577130224abd1f0
Suroj130 <sandipbera.ind@gmail.com>
2026-01-29 14:24:04 +0530

    Update Youtube.py

--- AnonXMusic/platforms/Youtube.py
+++ AnonXMusic/platforms/Youtube.py
@@ -9,69 +9,63 @@ if not os.path.exists(DOWNLOAD_DIR):
     os.makedirs(DOWNLOAD_DIR)
 
 
-ydl_opts_audio = {
-    "format": "bestaudio/best",
-    "outtmpl": f"{DOWNLOAD_DIR}/%(id)s.%(ext)s",
-    "quiet": True,
-    "nocheckcertificate": True,
-    "ignoreerrors": True,
-    "no_warnings": True,
-    "geo_bypass": True,
-    "postprocessors": [{
-        "key": "FFmpegExtractAudio",
-        "preferredcodec": "mp3",
-        "preferredquality": "192",
-    }],
-}
+class YouTubeAPI:
+    def __init__(self):
+        pass
 
-ydl_opts_video = {
-    "format": "bestvideo+bestaudio/best",
-    "outtmpl": f"{DOWNLOAD_DIR}/%(id)s.%(ext)s",
-    "quiet": True,
-    "nocheckcertificate": True,
-    "ignoreerrors": True,
-    "no_warnings": True,
-    "geo_bypass": True,
-}
+    async def download(self, link, mystic=None, video=False, **kwargs):
+        loop = asyncio.get_event_loop()
 
+        if video:
+            return await loop.run_in_executor(None, self._video, link), False
+        else:
+            return await loop.run_in_executor(None, self._audio, link), False
 
-async def _run_ydl(opts, url):
-    loop = asyncio.get_event_loop()
-    return await loop.run_in_executor(None, lambda: _ydl_download(opts, url))
+    def _audio(self, link):
+        ydl_opts = {
+            "format": "bestaudio/best",
+            "outtmpl": f"{DOWNLOAD_DIR}/%(id)s.%(ext)s",
+            "quiet": True,
+            "nocheckcertificate": True,
+            "ignoreerrors": True,
+            "no_warnings": True,
+            "geo_bypass": True,
+            "postprocessors": [
+                {
+                    "key": "FFmpegExtractAudio",
+                    "preferredcodec": "mp3",
+                    "preferredquality": "192",
+                }
+            ],
+        }
 
+        with yt_dlp.YoutubeDL(ydl_opts) as ydl:
+            info = ydl.extract_info(link, download=True)
+            vid = info["id"]
 
-def _ydl_download(opts, url):
-    with yt_dlp.YoutubeDL(opts) as ydl:
-        info = ydl.extract_info(url, download=True)
-        return info.get("id")
+        files = glob.glob(f"{DOWNLOAD_DIR}/{vid}.*")
+        if not files:
+            raise FileNotFoundError("Audio file not found")
 
+        return files[0]
 
-async def download_audio(url):
-    video_id = await _run_ydl(ydl_opts_audio, url)
-    files = glob.glob(f"{DOWNLOAD_DIR}/{video_id}.*")
-    if not files:
-        raise FileNotFoundError("Audio file not found")
-    return files[0]
+    def _video(self, link):
+        ydl_opts = {
+            "format": "bestvideo+bestaudio/best",
+            "outtmpl": f"{DOWNLOAD_DIR}/%(id)s.%(ext)s",
+            "quiet": True,
+            "nocheckcertificate": True,
+            "ignoreerrors": True,
+            "no_warnings": True,
+            "geo_bypass": True,
+        }
 
+        with yt_dlp.YoutubeDL(ydl_opts) as ydl:
+            info = ydl.extract_info(link, download=True)
+            vid = info["id"]
 
-async def download_video(url):
-    video_id = await _run_ydl(ydl_opts_video, url)
-    files = glob.glob(f"{DOWNLOAD_DIR}/{video_id}.*")
-    if not files:
-        raise FileNotFoundError("Video file not found")
-    return files[0]
+        files = glob.glob(f"{DOWNLOAD_DIR}/{vid}.*")
+        if not files:
+            raise FileNotFoundError("Video file not found")
 
-
-# MAIN FUNCTION (play command uses this)
-async def get_youtube(url, video=False):
-    """
-    Returns:
-    file_path, direct(False)
-    """
-
-    if video:
-        file = await download_video(url)
-    else:
-        file = await download_audio(url)
-
-    return file, False
+        return files[0]
